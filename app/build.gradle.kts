317692b62d5eb1c0f7335409040ac4956a34a8f2
Samkhan Chhayamony <monysamkhanchhaya@gmail.com>
2026-05-02 10:35:49 +0700

    Initial Commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,6 +36,7 @@ android {
 dependencies {
     implementation(libs.appcompat)
     implementation(libs.material)
+    implementation(libs.cardview)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
