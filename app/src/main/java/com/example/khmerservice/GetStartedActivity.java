9879da10f3ca10c8d9d2865c1784cf7cb7eb7f36
rothakarachna <h.rothakarachna@gmail.com>
2026-06-29 02:44:16 +0700

    updates on splash and start activities

---
+++ app/src/main/java/com/example/khmerservice/GetStartedActivity.java
@@ -0,0 +1,23 @@
+package com.example.khmerservice;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class GetStartedActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+
+        Button btnGetStarted = findViewById(R.id.btn_get_start);
+
+        btnGetStarted.setOnClickListener(v -> {
+            Intent intent = new Intent(GetStartedActivity.this, HomepageActivity.class);
+            startActivity(intent);
+            finish();
+        });
+    }
+}
